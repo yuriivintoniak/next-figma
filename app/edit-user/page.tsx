2f7ba2c8bcc280b2b578fa849f228b79d1ee9791
yuriivintoniak <you@example.com>
2024-12-29 22:41:20 +0200

    feat: create UI for EditUserForm component with select dropdowns

--- app/edit-user/page.tsx
+++ app/edit-user/page.tsx
@@ -1,9 +1,9 @@
+import EditUserForm from "@/components/EditUserForm/EditUserForm";
+
 export default function EditUser() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <h1 className="font-bold text-3xl">
-        Edit User
-      </h1>
+      <EditUserForm />
     </div>
   );
 }
